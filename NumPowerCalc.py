c552002663c92d4cc0b98fc39b40b6f05abadb8c
Aditya singh <143517499+Drake090@users.noreply.github.com>
2026-02-26 00:03:53 +0530

    Create NumPowerCalc.py

---
+++ NumPowerCalc.py
@@ -0,0 +1,6 @@
+a = int(input("enter the number:"))
+
+print("The square of the number is:", a ** 2)
+print("the squre root of the number is:", a ** 0.5)
+print("the cube of the number is:", a ** 3)
+print("the cube root of the number is:",a ** (1/3))
