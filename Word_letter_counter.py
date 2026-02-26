3de27f187a8690bcc478740991ccd5dcc098aa96
Aditya singh <143517499+Drake090@users.noreply.github.com>
2026-02-26 20:51:52 +0530

    Word and Letter counter program.

---
+++ Word_letter_counter.py
@@ -0,0 +1,7 @@
+a = input("Enter sentence:")
+words = a.split()
+
+print("The total number of words in the sentence is:", len(words))
+
+print("The total number of characters in the sentence is:", len(a))
+
--- main.py
+++
@@ -1,3 +0,0 @@
-a = """hi my  name   is  aditya  singh,
- and  i  am  learning python""" 
-print(a.replace("  ", " "))
