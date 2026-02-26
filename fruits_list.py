e82a9d8e48ff251fc2fc67fc5fc8fe0797131671
Aditya singh <143517499+Drake090@users.noreply.github.com>
2026-02-26 22:44:39 +0530

    Create fruits_list.py

---
+++ fruits_list.py
@@ -0,0 +1,8 @@
+fruits = []
+f1 = input("enter the name of the fruit:")
+fruits.append(f1)
+f2 = input("enter the name of the fruit:")
+fruits.append(f2)
+f3 = input("enter the name of the fruit:")
+fruits.append(f3)
+print("the list of fruits is:", fruits)
