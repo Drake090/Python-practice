66c72b6f55b00c5d4dfde08e9eb15631ad80f7e9
Aditya singh <143517499+Drake090@users.noreply.github.com>
2026-02-26 22:56:56 +0530

    Create MarksSorter.py

---
+++ MarksSorter.py
@@ -0,0 +1,9 @@
+marks = []
+f1 = int(input("enter the marks:"))
+marks.append(f1)
+f2 = int(input("enter the marks:"))
+marks.append(f2)
+f3 = int(input("enter the marks:"))
+marks.append(f3)
+marks.sort()
+print("Marks obtained by student is:", marks)
